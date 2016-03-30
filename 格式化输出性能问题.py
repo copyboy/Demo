d6cfaea1413ce8a6740eea92fae3d95f080951ef
qdzhang <copy_boy@126.com>
2016-03-30 23:28:02 +0800

    格式化字符串输出简单测试

---
+++ 格式化输出性能问题.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+__author__ = 'Yuriy'
+
+import time
+
+user = 'zlzg'
+start = time.clock()
+
+for i in range(100):
+	print 'it is a test input',user
+
+usetime = time.clock() - start
+
+
+started = time.clock()
+
+for i in range(100):
+	print 'it is a test input %s' % user
+
+usedtime = time.clock() - started
+
+print usetime
+print usedtime

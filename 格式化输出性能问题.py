681cf3cd4a5ebb3c7dc1a043f7a349e791576b4e
qdzhang <copy_boy@126.com>
2016-03-30 23:35:27 +0800

    print数据量增大，性能基本差不多了

--- 格式化输出性能问题.py
+++ 格式化输出性能问题.py
@@ -6,7 +6,7 @@ import time
 user = 'zlzg'
 start = time.clock()
 
-for i in range(100):
+for i in range(1000):
 	print 'it is a test input',user
 
 usetime = time.clock() - start
@@ -14,7 +14,7 @@ usetime = time.clock() - start
 
 started = time.clock()
 
-for i in range(100):
+for i in range(1000):
 	print 'it is a test input %s' % user
 
 usedtime = time.clock() - started
